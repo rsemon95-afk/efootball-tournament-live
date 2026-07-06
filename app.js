febc45784e7325ea98f99dc82a9d0d28b2690da8
rsemon95-afk <rsemon95@gmail.com>
2026-07-06 21:42:57 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
+async function loadTeams() {
 
+    const { data, error } = await supabase
+        .from("teams")
+        .select("*")
+        .order("points", { ascending: false });
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    const tbody = document.querySelector("#pointsTable tbody");
+    tbody.innerHTML = "";
+
+    data.forEach(team => {
+
+        tbody.innerHTML += `
+        <tr>
+            <td>${team.name}</td>
+            <td>${team.played}</td>
+            <td>${team.win}</td>
+            <td>${team.draw}</td>
+            <td>${team.loss}</td>
+            <td>${team.gf}</td>
+            <td>${team.ga}</td>
+            <td>${team.gd}</td>
+            <td><b>${team.points}</b></td>
+        </tr>
+        `;
+
+    });
+
+}
+
+loadTeams();
+
+supabase
+.channel("teams-live")
+.on(
+    "postgres_changes",
+    {
+        event: "*",
+        schema: "public",
+        table: "teams"
+    },
+    () => {
+        loadTeams();
+    }
+)
+.subscribe();
