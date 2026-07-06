3d164037af6a56ab46ef7f838301a24f9cbecde4
rsemon95-afk <rsemon95@gmail.com>
2026-07-06 21:41:54 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,4 @@
-
+const supabase = window.supabase.createClient(
+'https://vbmcjmozouojqgjlpyjb.supabase.co',
+'তোমার_Anon_Public_Key'
+);eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZibWNqbW96b3VvanFnamxweWpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMzc2NTIsImV4cCI6MjA5ODkxMzY1Mn0.D50RAqxpye7NF_z_9ODo2s28Nl4TwB1DmcJYxI_2eqs
